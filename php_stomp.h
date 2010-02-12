4fcd961e8bfba8fc831ecee1f34756ecef01d495
Pierrick Charron <pierrick@php.net>
2010-02-12 04:55:47 +0000

    Bump to stable

--- php_stomp.h
+++ php_stomp.h
@@ -29,9 +29,9 @@ typedef struct _stomp_object {
 } stomp_object_t; 
 
 #define PHP_STOMP_EXTNAME                       "Stomp"
-#define PHP_STOMP_MAJOR_VERSION                 "0"
-#define PHP_STOMP_MINOR_VERSION                 "4"
-#define PHP_STOMP_PATCH_VERSION                 "1"
+#define PHP_STOMP_MAJOR_VERSION                 "1"
+#define PHP_STOMP_MINOR_VERSION                 "0"
+#define PHP_STOMP_PATCH_VERSION                 "0"
 #define PHP_STOMP_VERSION_STATUS                ""
 #define PHP_STOMP_VERSION                       PHP_STOMP_MAJOR_VERSION "." PHP_STOMP_MINOR_VERSION "." PHP_STOMP_PATCH_VERSION PHP_STOMP_VERSION_STATUS
 
