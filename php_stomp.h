8aece537c17d193b03001bdfb07cd4495281ffa6
Pierrick Charron <pierrick@webstart.fr>
2014-12-07 21:57:57 -0500

    1.0.6 => 1.0.7-dev

--- php_stomp.h
+++ php_stomp.h
@@ -31,8 +31,8 @@ typedef struct _stomp_object {
 #define PHP_STOMP_EXTNAME                       "Stomp"
 #define PHP_STOMP_MAJOR_VERSION                 "1"
 #define PHP_STOMP_MINOR_VERSION                 "0"
-#define PHP_STOMP_PATCH_VERSION                 "6"
-#define PHP_STOMP_VERSION_STATUS                ""
+#define PHP_STOMP_PATCH_VERSION                 "7"
+#define PHP_STOMP_VERSION_STATUS                "-dev"
 #define PHP_STOMP_VERSION                       PHP_STOMP_MAJOR_VERSION "." PHP_STOMP_MINOR_VERSION "." PHP_STOMP_PATCH_VERSION PHP_STOMP_VERSION_STATUS
 
 #define PHP_STOMP_RES_NAME                      "stomp connection"
